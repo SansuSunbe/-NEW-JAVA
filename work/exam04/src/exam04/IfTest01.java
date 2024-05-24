0d6efc8ee52a14dabaa2bf8f966e0cbeaeb742bb
SansuSunbe <149463418+SansuSunbe@users.noreply.github.com>
2024-05-24 21:10:08 +0900

    Add files via upload

---
+++ work/exam04/src/exam04/IfTest01.java
@@ -0,0 +1,20 @@
+package exam04;
+
+public class IfTest01 {
+	
+	public static void main(String[] args) {
+		
+		int score = 50;
+		
+		// 점수가 60점 이상이면 합격
+		if(score >= 60) {
+			// 참일 때 실행
+			System.out.println("합격");
+		} else {
+			// 거짓일 때 실행
+			System.out.println("불합격");
+		}
+		
+	}
+
+}
---
+++ work/exam04/src/exam04/IfTest03.java
@@ -0,0 +1,28 @@
+package exam04;
+
+import java.util.Scanner;
+
+public class IfTest03 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("점수 입력 : ");
+		int score = scan.nextInt();
+
+		if (score >= 90) {
+			System.out.println("점수 : " + score + ", 학점 : A");
+		} else if (score >= 80) {
+			System.out.println("점수 : " + score + ", 학점 : B");
+		} else if (score >= 70) {
+			System.out.println("점수 : " + score + ", 학점 : C");
+		} else {
+			System.out.println("점수 : " + score + ", 학점 : F");
+		}
+		
+		scan.close();
+
+	}
+
+}
