08e5ec3272edc5c603b85cc1790a82e3944a5354
SansuSunbe <149463418+SansuSunbe@users.noreply.github.com>
2024-07-05 22:16:31 +0900

    Add files via upload

---
+++ work/exam11/src/exam11/DeepCopyOfArrayExam01.java
@@ -0,0 +1,36 @@
+package exam11;
+
+public class DeepCopyOfArrayExam01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr01 = { 1, 3, 5, 7, 9 };
+		int[] arr02 = new int[5];
+
+		// 첫 번째 배열의 값들을 loop를 이용해서 대입
+		for(int i = 0; i < arr01.length; i++) {
+			arr02[i] = arr01[i];
+		}
+		
+		arr02[1] = 11;
+		arr01[0] = 8;
+
+		System.out.println();
+		System.out.println("arr02 값 출력 : ");
+
+		for (int i = 0; i < arr01.length; i++) {
+			System.out.print(arr02[i] + "\t");
+		}
+
+		System.out.println();
+		System.out.println("arr01 값 출력 : ");
+
+		for (int i = 0; i < arr01.length; i++) {
+			System.out.print(arr01[i] + "\t");
+
+		}
+ 
+		
+	}
+}
